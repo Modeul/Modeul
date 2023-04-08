675db5caf855cf7658e1c4963d52288df38be0bc
sungmin <kimmoksil@gmail.com>
2023-04-08 18:46:14 +0900

    [feat] 페이징을 위한 list api 추가

--- src/main/java/com/modeul/web/controller/api/StuffController.java
+++ src/main/java/com/modeul/web/controller/api/StuffController.java
@@ -43,7 +43,8 @@ public class StuffController {
 			@RequestParam(name="c", required=false) Long categoryId
 			){
 		
-		List<StuffView> list = service.getViewAll(categoryId, page);
+//		List<StuffView> list = service.getViewAll(categoryId, page);
+		List<StuffView> list = service.getRecentViewList(categoryId, page);
 		
 		return list;
 	}
@@ -65,7 +66,7 @@ public class StuffController {
 		
 		Stuff stuff = service.getById(id);
 		
-		String categoryName = categoryService.getNameById(stuff.getId());
+//		String categoryName = categoryService.getNameById(stuff.getId());
 		
 		return stuff;
 	}
