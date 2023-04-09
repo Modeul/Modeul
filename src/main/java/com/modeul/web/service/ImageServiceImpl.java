f863932bc2442957e6ba7fa4cac1f55980bd87ed
wogjs0911 <wogjs0911@naver.com>
2023-04-09 23:28:10 +0900

    [feat] list 페이지 공구상품 목록별 이미지 출력, 3중 join view 테이블로 변경

--- src/main/java/com/modeul/web/service/ImageServiceImpl.java
+++ src/main/java/com/modeul/web/service/ImageServiceImpl.java
@@ -1,5 +1,7 @@
 package com.modeul.web.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +22,11 @@ public class ImageServiceImpl implements ImageService{
 	@Override
 	public Image getbyId(Long stuffId) {
 		return repository.findById(stuffId);
+	}
+
+	@Override
+	public List<Image> getList() {
+		return repository.findAll();
 	}
 
 }
