3ea9182218d2337e9cef13832f47e8070e0cbd9d
SONCHUNG <99730398+SONCHUNG@users.noreply.github.com>
2023-04-10 00:17:34 +0900

    [feat] email 전송 기능 구현
    MailConfig, MailService, MailServiceImpl, MemberController

--- src/main/java/com/modeul/web/controller/api/MemberController.java
+++ src/main/java/com/modeul/web/controller/api/MemberController.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.modeul.web.entity.Member;
+import com.modeul.web.service.MailServiceImpl;
 import com.modeul.web.service.MemberService;
 
 @RestController("apiMemberController")
@@ -15,6 +18,8 @@ public class MemberController {
 	
 	@Autowired
 	private MemberService service;
+	@Autowired
+	private MailServiceImpl mailService;
 	
 	@PostMapping
 	public int addMember(@RequestBody Member member) {
@@ -22,5 +27,15 @@ public class MemberController {
 		int memberCount = service.addMember(member);
 		return memberCount;
 	}
+
+	// 이메일 인증 (RequestParam)
+	@PostMapping("mailConfirm")
+	@ResponseBody
+	public String mailConfirm(@RequestParam("email") String email) throws Exception{
+		String code = mailService.sendConfirmMessage(email);
+		System.out.println("인증코드 : " + code);
+		return code;
+	}
+
 
 }
